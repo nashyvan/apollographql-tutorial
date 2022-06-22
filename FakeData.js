6ae80f9270f85e774ab425cde5a48d5cd748ea5e
yariknashivan <yarik.nashivan@gmail.com>
2022-06-22 23:59:58 +0300

    first commit

---
+++ FakeData.js
@@ -0,0 +1,22 @@
+const UserList = [
+  {
+    id: 1,
+    name: 'Nick',
+    age: 42,
+    nationality: 'Ukraine',
+  },
+  {
+    id: 2,
+    name: 'John',
+    age: 24,
+    nationality: 'Germany',
+  },
+  {
+    id: 1,
+    name: 'Mike',
+    age: 20,
+    nationality: 'United States',
+  },
+]
+
+module.exports = { UserList };
